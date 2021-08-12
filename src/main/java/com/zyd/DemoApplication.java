ded597a8d860f04c5b7de56629fe0239821be7f1
octopus996 <62587372+octopus996@users.noreply.github.com>
2021-08-12 21:40:56 +0800

    1

--- src/main/java/com/zyd/DemoApplication.java
+++ src/main/java/com/zyd/DemoApplication.java
@@ -3,6 +3,7 @@ package com.zyd;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @MapperScan("com.zyd.*.dao")
