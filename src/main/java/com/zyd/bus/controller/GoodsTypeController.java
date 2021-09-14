2ed6364e7918e33ef041368abbad0bcda93a9366
Administrator <739861073@qq.com>
2021-09-14 14:07:17 +0800

    1

--- src/main/java/com/zyd/bus/controller/GoodsTypeController.java
+++ src/main/java/com/zyd/bus/controller/GoodsTypeController.java
@@ -1,6 +1,7 @@
 package com.zyd.bus.controller;
 
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -14,12 +15,13 @@ import com.zyd.common.util.TreeNode;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -70,7 +72,7 @@ public class GoodsTypeController {
     @RequestMapping("goodstypelist")
     public DataGridViewResult goodstypelist(GoodsTypeVo goodsTypeVo){
         //创建分页构造器
-        IPage<GoodsType> page=new Page<>();
+        IPage<GoodsType> page=new Page<>(goodsTypeVo.getPage(),goodsTypeVo.getLimit());
         //创建条件构造器
         QueryWrapper<GoodsType> queryWrapper=new QueryWrapper<>();
         //模糊查询商品名称
@@ -119,6 +121,55 @@ public class GoodsTypeController {
         }else {
             return SystemConstant.ADD_ERROR;
         }
+    }
+
+    /**
+     * 检查节点下有无子节点
+     * @param id
+     * @return
+     */
+    @RequestMapping("/checkgoodstypeHasChildren")
+    public String checkgoodstypeHasChildren(int id){
+        //创建一个map集合存放值
+        Map<String,Object> map =new HashMap<>();
+        //创建条件构造器
+        QueryWrapper<GoodsType> queryWrapper=new QueryWrapper<>();
+        queryWrapper.eq("pid",id);
+        if (goodsTypeService.count(queryWrapper)>0){
+            map.put(SystemConstant.EXIST,true);
+            map.put(SystemConstant.MESSAGE,"存在子节点，无法删除！");
+        }else {
+            map.put(SystemConstant.EXIST,false);
+        }
+
+        return JSON.toJSONString(map);
+    }
+
+    /**
+     * 通过id删除节点
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping("/deleteById")
+    public JSONResult deleteById(int id){
+        if (goodsTypeService.removeById(id)){
+            return SystemConstant.DELETE_SUCCESS;
+        }
+        return SystemConstant.DELETE_ERROR;
+    }
+
+    /**
+     * 修改商品类型
+     * @param goodsType
+     * @return
+     */
+    @RequestMapping("/updategoodstype")
+    public JSONResult updategoodstype(GoodsType goodsType){
+        if (goodsTypeService.updateById(goodsType)){
+            return SystemConstant.UPDATE_SUCCESS;
+        }
+        return SystemConstant.UPDATE_ERROR;
     }
 }
 
