e1ff44288e211835ee7a9e2fde93c74e42b93c4b
Administrator <739861073@qq.com>
2021-10-12 17:19:31 +0800

    1

--- src/main/java/com/zyd/config/MyWebMvcConfiguration.java
+++ src/main/java/com/zyd/config/MyWebMvcConfiguration.java
@@ -1,4 +1,4 @@
-package com.zyd.sys.config;
+package com.zyd.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
